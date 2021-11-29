b085bb125c727e38eba280620e6cbfa4e27b0d4c
Timur Cakmakoglu <tcakmako@k1m04s06.42kocaeli.com.tr>
2021-11-29 11:45:24 +0300

    finished pc01 ex08

--- piscine_c01/ex08/ft_sort_int_tab.c
+++ piscine_c01/ex08/ft_sort_int_tab.c
@@ -1,49 +1,25 @@
-void	locate_min(int *tab, int tab_size, int min_index, int *holder)
-{
-	int	index;
-
-	index = min_index;
-	while (index < tab_size)
-	{
-		if(++index < tab_size && tab[index] < holder[0])
-		{
-			holder[0] = tab[index];
-			holder[1] = index;
-		}
-	}
-}
-
-void	swap_min(int *tab, int tab_size, int min_index, int *holder)
-{
-	int	index;
-
-	index = min_index;
-	while (index < tab_size)
-	{
-		if (holder[0] < tab[index])
-		{
-			tab[holder[1]] = tab[index];
-			tab[index] = holder[0];
-			min_index++;
-			break;
-		}
-		index++;
-	}
-}
-
 void	ft_sort_int_tab(int *tab, int size)
 {
 	int	min_index;
-	int	index;
+	int	var_index;
 	int	min_holder[2];
 
 	min_index = 0;
-	index = 0;
-	while (index < size)
+	while (min_index < size)
 	{
+		var_index = min_index;
 		min_holder[0] = 2147483647;
-		locate_min(tab, size, min_index, min_holder);
-		swap_min(tab, size, min_index, min_holder);
-		index = min_index;
+		while (var_index < size)
+		{
+			if (tab[var_index] < min_holder[0])
+			{
+				min_holder[0] = tab[var_index];
+				min_holder[1] = var_index;
+			}
+			var_index++;
+		}
+		tab[min_holder[1]] = tab[min_index];
+		tab[min_index] = min_holder[0];
+		min_index++;
 	}
 }
