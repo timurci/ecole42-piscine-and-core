4409f0082dfcc38f118d5f6f2a42eb0c53ccc442
timurci <timurcakmakoglu@gmail.com>
2022-01-06 17:46:11 +0300

    added pisc c09

---
+++ piscine_c09/ex00/libft_creator.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+gcc -c ft_putchar.c
+gcc -c ft_swap.c
+gcc -c ft_putstr.c
+gcc -c ft_strlen.c
+gcc -c ft_strcmp.c
+ar rcs libft.a ft_putchar.o ft_swap.o ft_putstr.o ft_strlen.o ft_strcmp.o
+rm ft_putchar.o ft_swap.o ft_putstr.o ft_strlen.o ft_strcmp.o
