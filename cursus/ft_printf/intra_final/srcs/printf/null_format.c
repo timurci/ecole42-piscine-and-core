714e324214656739154adf95fc02b0c265541c6b
Timur Cakmakoglu <tcakmako@k2m06s01.42kocaeli.com.tr>
2022-03-27 18:36:50 +0300

    updated printf

--- cursus/ft_printf/intra_final/srcs/printf/null_format.c
+++ cursus/ft_printf/intra_final/srcs/printf/null_format.c
@@ -6,7 +6,7 @@
 /*   By: tcakmako tcakmako@student.42kocaeli.com.t  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/27 15:22:56 by tcakmako          #+#    #+#             */
-/*   Updated: 2022/03/27 15:36:13 by tcakmako         ###   ########.fr       */
+/*   Updated: 2022/03/27 18:34:58 by tcakmako         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,28 +15,12 @@
 char	*nullp_checker(char *s, t_shape *sh, char type)
 {
 	int		scan;
-	char	*news;
 
 	scan = 1;
 	if (*s == 0 && sh->w1 == 1 && type != 'i')
 	{
 		sh->w1 = 0;
 		return (s);
-	}
-	while (scan < sh->w1)
-	{
-		if (s[scan] == 0)
-		{
-			write(1, s, ft_strlen(s));
-			write(1, &s[scan], 1);
-			news = malloc(sizeof(char) * (sh->w1 - scan));
-			news[sh->w1 - scan] = 0;
-			ft_memcpy(news, &s[scan + 1], sh->w1 - scan - 1);
-			sh->w1 = ft_strlen(news);
-			free (s);
-			return (news);
-		}
-		scan++;
 	}
 	return (s);
 }
