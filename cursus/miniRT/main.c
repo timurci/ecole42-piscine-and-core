678ef12b34cfb1d2c5d544b4e607380ac632a873
timurci <timurcakmakoglu@gmail.com>
2023-05-28 22:43:19 +0300

    norm adjustment

--- cursus/miniRT/main.c
+++ cursus/miniRT/main.c
@@ -6,7 +6,7 @@
 /*   By: tcakmako <tcakmako@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/15 13:51:46 by tcakmako          #+#    #+#             */
-/*   Updated: 2023/05/26 17:12:35 by tcakmako         ###   ########.fr       */
+/*   Updated: 2023/05/28 22:40:28 by tcakmako         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,30 +45,6 @@
 # define ASPECT_RATIO 1.77777777f
 #endif
 
-/*static void	add_objects(t_objects *objects, const float aspect_ratio)
-{
-	objects->camera = camera_set(aspect_ratio, 66, vector3_set(0, 0, -1), point3_set(0, 0, 5));
-
-	objects->sphere = add_sphere(objects->sphere, point3_set(0, 0, -1), 0.5, color3_set(0,1,0));
-	objects->sphere = add_sphere(objects->sphere, point3_set(0, -100.5, -1), 100, color3_set(0.8, 0.8, 0.0));
-	objects->sphere = add_sphere(objects->sphere, point3_set(-1, 1, -2), 0.5, color3_set(0.7, 0.3, 0.3));
-	//objects->sphere = add_sphere(objects->sphere, point3_set(1, -0.6, -2), 0.5, color3_set(0.5, 0.5, 0.5));
-	//objects->sphere = add_sphere(objects->sphere, point3_set(4, 5, -5), 1, color3_set(0.5, 0.5, 0.5));
-
-	//objects->plane = add_plane(objects->plane, point3_set(1,0,-10), vector3_set(1, 0, 0.7), color3_set(0.1, 0.1, 1));
-	//objects->plane = add_plane(objects->plane, point3_set(-1,0,-10), vector3_set(-1, 0, 0.7), color3_set(1, 0.1, 0.1));
-	
-	objects->cylinder = add_cylinder(objects->cylinder, point3_set(-1, 2, -5),
-							cylinder_params(1, 2, vector3_set(0.7, 0.2, 1)), color3_set(1, 0, 1));
-	objects->cylinder = add_cylinder(objects->cylinder, point3_set(2, 1, -5),
-							cylinder_params(1, 5, vector3_set(-0.7, 0.2, 1)), color3_set(1, 0.2, 0));
-	objects->cylinder = add_cylinder(objects->cylinder, point3_set(2, 2, -5),
-							cylinder_params(1, 5, vector3_set(-0.7, 0.2, -1)), color3_set(0, 0.5, 1));
-
-	objects->point_light = set_point_light(color3_set(1, 1, 1), 1.0f, point3_set(-4, 3, 1));
-	objects->ambient_light = set_ambient_light(color3_set(0.3, 0.5, 1.0), 0.5f);
-}*/
-
 static bool	check_argc(const int argc)
 {
 	if (argc < 2)
@@ -88,39 +64,23 @@ int	main(int argc, char **argv)
 	t_objects	*objects;
 	t_mlx_core	*mlx_core;
 
-
 	if (check_argc(argc))
 		return (1);
 	objects = parse(argv[1], ASPECT_RATIO);
 	if (!objects)
 		return (1);
-
 	mlx_core = t_mlx_init_core(width, height, "miniRT (Test)");
 	mlx_loop_hook(mlx_core->ptr, t_mlx_loop_hook, mlx_core);
 	mlx_key_hook(mlx_core->win, t_mlx_key_hook, mlx_core);
 	mlx_mouse_hook(mlx_core->win, t_mlx_mouse_hook, mlx_core);
 	mlx_hook(mlx_core->win, 17, 0, terminate, mlx_core);
-
-	//objects = malloc(sizeof(*objects));
-
-	//objects->sphere = NULL;
-	//objects->plane = NULL;
-	//objects->cylinder = NULL;
-	
-	//add_objects(objects, aspect_ratio);
-	// line 84-90 should be replaced by parser function.
-	
 	mlx_core->objects = objects;
-
 	draw_mlx_wo_sampling(mlx_core->frame[0], objects);
 	draw_mlx_wo_sampling(mlx_core->frame[1], objects);
-	
 	t_mlx_core_switch_frame(mlx_core);
-
 	mlx_loop(mlx_core->ptr);
 	t_mlx_destroy_core(mlx_core);
 	destroy_objects(objects);
 	free(objects);
-	
 	return (0);
 }
--- cursus/miniRT/ppm/main.c
+++ cursus/miniRT/ppm/main.c
@@ -6,7 +6,7 @@
 /*   By: tcakmako <tcakmako@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/15 13:51:46 by tcakmako          #+#    #+#             */
-/*   Updated: 2023/05/22 16:32:03 by tcakmako         ###   ########.fr       */
+/*   Updated: 2023/05/28 22:41:53 by tcakmako         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,30 +41,6 @@
 # define ASPECT_RATIO 1.77777777f
 #endif
 
-/*static void add_objects(t_objects *objects, const float aspect_ratio)
-{
-	objects->camera = camera_set(aspect_ratio, 66, vector3_set(0, 0, -1), point3_set(0, 0, 5));
-
-	objects->sphere = add_sphere(objects->sphere, point3_set(0, 0, -1), 0.5, color3_set(0,1,0));
-	objects->sphere = add_sphere(objects->sphere, point3_set(0, -100.5, -1), 100, color3_set(0.8, 0.8, 0.0));
-	objects->sphere = add_sphere(objects->sphere, point3_set(-1, 1, -2), 0.5, color3_set(0.7, 0.3, 0.3));
-	//objects->sphere = add_sphere(objects->sphere, point3_set(1, -0.6, -2), 0.5, color3_set(0.5, 0.5, 0.5));
-	//objects->sphere = add_sphere(objects->sphere, point3_set(4, 5, -5), 1, color3_set(0.5, 0.5, 0.5));
-
-	//objects->plane = add_plane(objects->plane, point3_set(1,0,-10), vector3_set(1, 0, 0.7), color3_set(0.1, 0.1, 1));
-	//objects->plane = add_plane(objects->plane, point3_set(-1,0,-10), vector3_set(-1, 0, 0.7), color3_set(1, 0.1, 0.1));
-	
-	objects->cylinder = add_cylinder(objects->cylinder, point3_set(-1, 2, -5),
-							cylinder_params(1, 2, vector3_set(0.7, 0.2, 1)), color3_set(1, 0, 1));
-	objects->cylinder = add_cylinder(objects->cylinder, point3_set(2, 1, -5),
-							cylinder_params(1, 5, vector3_set(-0.7, 0.2, 1)), color3_set(1, 0.2, 0));
-	objects->cylinder = add_cylinder(objects->cylinder, point3_set(2, 2, -5),
-							cylinder_params(1, 5, vector3_set(-0.7, 0.2, -1)), color3_set(0, 0.5, 1));
-
-	objects->point_light = set_point_light(color3_set(1, 1, 1), 1.0f, point3_set(-4, 3, 1));
-	objects->ambient_light = set_ambient_light(color3_set(0.3, 0.5, 1.0), 0.5f);
-}*/
-
 static bool	check_argc(const int argc)
 {
 	if (argc < 2)
@@ -86,30 +62,17 @@ int	main(int argc, char **argv)
 
 	img.height = height;
 	img.width = width;
-	
-	//t_objects	*objects = malloc(sizeof(*objects));
-
-	//objects->sphere = NULL;
-	//objects->plane = NULL;
-	//objects->cylinder = NULL;
-	
-	//add_objects(objects, aspect_ratio);
-
 	if (check_argc(argc))
 		return (1);
 	objects = parse(argv[1], ASPECT_RATIO);
 	if (!objects)
 		return (1);
-
 	printf("P3\n%d %d\n255\n", width, height);
-
 	if (SAMPLES_PER_PIXEL == 1)
 		draw_ppm_wo_sampling(&img, objects);
 	else
 		draw_ppm_w_sampling(&img, objects);
-	
 	destroy_objects(objects);
 	free(objects);
-	
 	return (0);
 }
